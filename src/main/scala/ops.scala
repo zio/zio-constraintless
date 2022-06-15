25f34b0e15686740c38f51f3ae9e7042a45fbb07
Afsal Thaj <afsal.taj06@gmail.com>
2022-06-15 14:53:45 +1000

    Change ops to have ratio value

--- src/main/scala/ops.scala
+++ src/main/scala/ops.scala
@@ -5,29 +5,32 @@ import Ops.Zip
   */
 sealed trait Ops[As <: HList, F[_], O] { self =>
 
-  def zip[B](that: Ops[As, F, B]): Ops[As, F, (O, B)] =
-    Ops.Zip(self, that)
+  def <*>[B](
+      that: Ops[As, F, B]
+  )(implicit elem1: O Elem As, elem2: B Elem As): Ops[As, F, (O, B)] =
+    Ops.Zip(self, that, elem1, elem2)
 
-  def /(that: Ops[As, F, O]): Ops[As, F, O] =
-    Ops.Ratio(self, that)
+  def /(that: Ops[As, F, O])(implicit elem: O Elem As): Ops[As, F, O] =
+    Ops.Ratio(self, that, elem)
 }
 
 object Ops {
 
-  final case class Pure[As <: HList, F[_], A](fa: F[A]) extends Ops[As, F, A]
+  final case class Pure[As <: HList, F[_], A](fa: F[A], constraint: A Elem As)
+      extends Ops[As, F, A]
 
   final case class Zip[As <: HList, F[_], A, B](
       left: Ops[As, F, A],
-      right: Ops[As, F, B]
+      right: Ops[As, F, B],
+      elem1: A Elem As,
+      elem2: B Elem As
   ) extends Ops[As, F, (A, B)]
 
   final case class Ratio[As <: HList, F[_], B](
       left: Ops[As, F, B],
-      right: Ops[As, F, B]
+      right: Ops[As, F, B],
+      elem1: B Elem As
   ) extends Ops[As, F, B]
-
-  def pure[F[_], A](f: F[A]) =
-    Pure(f)
 
 }
 
@@ -44,8 +47,10 @@ object queryplanner {
   type ExecPlan[As <: HList, K, A] = Ops[As, Query[K, *], A]
 
   object ExecPlan {
-    def pure[As <: HList, K, A](query: Query[K, A]): ExecPlan[As, K, A] =
-      Ops.Pure[As, Query[K, *], A](query)
+    def pure[As <: HList, K, A](query: Query[K, A])(implicit
+        ev: A Elem As
+    ): ExecPlan[As, K, A] =
+      Ops.Pure[As, Query[K, *], A](query, ev)
   }
 
 }
@@ -56,10 +61,13 @@ object queryplannercompiler {
   type IO[K, A] = Map[K, A]
 
   // You can see the hidden information in this exec plan
-  def run[As <: HList, K, A](plan: ExecPlan[As, K, A]): IO[K, A] = {
+  def run[As <: HList, K, A](
+      plan: ExecPlan[As, K, A]
+  )(implicit mathOp: All[MathOp, As]): IO[K, A] = {
     def loop[B](plan: ExecPlan[As, K, B]): IO[K, B] = {
       plan match {
         case pure: Pure[As, Query[K, *], B] => ???
+
         case zip: Zip[As, Query[K, *], b, c] =>
           val map1 = loop(zip.left)
           val map2 = loop(zip.right)
@@ -78,16 +86,28 @@ object queryplannercompiler {
           val map1 = loop(ratio.left)
           val map2 = loop(ratio.right)
 
+          def zero[B](trap: All.Trap[MathOp, B]): B =
+            trap.ev.zero
+
           map1.map({ case (k, b) =>
             map2
               .get(k)
               .map({ b2 =>
                 (
                   k,
-                  (b.asInstanceOf[Int] / b2.asInstanceOf[Int]).asInstanceOf[B]
+                  mathOp.withElem[B, B](Proxy[As])(trap =>
+                    trap.ev.ratio(b, b2)
+                  )(ratio.elem1)
                 )
               })
-              .getOrElse((k, 0.asInstanceOf[B]))
+              .getOrElse(
+                (
+                  k,
+                  mathOp.withElem[B, B](Proxy[As])(trap => trap.ev.zero)(
+                    ratio.elem1
+                  )
+                )
+              )
           })
 
       }
@@ -100,19 +120,28 @@ object queryplannercompiler {
 
 object QueryPlannerSpec extends App {
   import queryplanner._
+  import HList._
 
-  def myPlan[As <: HList]: ExecPlan[As, String, Int] =
-    ExecPlan.pure(Api[(String, Int)]()) / ExecPlan.pure(Api[(String, Int)]())
+  type PlannerTypes = Int :: (Int, Int) :: HNil
+
+  val v1: ExecPlan[PlannerTypes, String, Int] =
+    ExecPlan.pure[PlannerTypes, String, Int](Api[(String, Int)]())
+
+  val v2: ExecPlan[PlannerTypes, String, Int] =
+    ExecPlan.pure[PlannerTypes, String, Int](Api[(String, Int)]())
+
+  def myPlan: ExecPlan[PlannerTypes, String, Int] =
+    v1 / v2
 
   // One way to get a tuple where the original query was (k, a) but program later produced (k, (a, a))
-  def zippedPlan[As <: HList]: ExecPlan[As, String, (Int, Int)] =
-    myPlan.zip(myPlan)
+  val zippedPlan: ExecPlan[PlannerTypes, String, (Int, Int)] =
+    myPlan <*> myPlan
 
   // This implies your query implementor need to know about how to get a (k, (a, a)) given a request
   // Ideally this make sense, because that leads to a scalable code base, where our values from data-source are bound to change
   // in future rather than sticking on to a specific value
   // However this scenairo implies, any Query[K, A] => Map[K, A], and A can take any shape ==> f: Query[K, *] ~> Map[K, *]
-  def zippedPlan_[As <: HList]: ExecPlan[As, String, (Int, Int)] =
+  def zippedPlan_ : ExecPlan[PlannerTypes, String, (Int, Int)] =
     ExecPlan.pure(Api[(String, (Int, Int))])
 
   val result = queryplannercompiler.run(zippedPlan)
