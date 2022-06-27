f2e9af28c34656d195a6ef0147b60d4a449d1ae9
Afsal Thaj <afsal.taj06@gmail.com>
2022-06-28 02:13:35 +1000

    Update problem.scala

--- docs/problem.scala
+++ docs/problem.scala
@@ -63,5 +63,7 @@ object MakeThisWork extends App {
   // Can you try and avoid having to summon manually how to add various tuples and still work with run method.
   // i.e, I should be able to simply do `run(addZipped)` instead of `run(addZipped)(add)`
   run(addZipped) // make it compile
+  run(added)
+  run(zipAdded)
   
 }
