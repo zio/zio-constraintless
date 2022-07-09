59ad73175e0be73d6e7126d32d26040c90e9647a
Afsal Thaj <afsal.taj06@gmail.com>
2022-07-10 03:00:22 +1000

    Remove Proxy

--- src/main/scala/thaj/constraintless/All.scala
+++ src/main/scala/thaj/constraintless/All.scala
@@ -3,7 +3,7 @@
 package thaj.constraintless
 
 trait All[C[_], As <: HList] {
-  def withElem[B, D](as: Proxy[As])(trap: All.Trap[C, B] => D)(implicit
+  def withElem[B, D](trap: All.Trap[C, B] => D)(implicit
       ev: B Elem As
   ): D
 }
@@ -21,8 +21,8 @@ object All {
       ev: All[C, As]
   ): All[C, A :: As] = new All[C, A :: As] {
     override def withElem[B, D](
-        as: Proxy[A :: As]
-    )(trap: Trap[C, B] => D)(implicit ev2: Elem[B, A :: As]): D =
+        trap: Trap[C, B] => D
+    )(implicit ev2: Elem[B, A :: As]): D =
       ev2.evidence match {
         case evidence: Evidence[B, A :: As] =>
           evidence match {
@@ -30,14 +30,14 @@ object All {
               trap(
                 Trap(c.asInstanceOf[C[B]])
               ) // Coz we have compile time evidence that B is infact A
-            case Tail(x) => ev.withElem(Proxy[As]())(trap)(x)
+            case Tail(x) => ev.withElem(trap)(x)
           }
       }
   }
 
   // The definition is slightly from what mentioned in the paper where it traverses hlist
   implicit def allHList2[C[_]]: All[C, HNil] = new All[C, HNil] {
-    override def withElem[B, D](as: Proxy[HNil])(trap: Trap[C, B] => D)(implicit
+    override def withElem[B, D](trap: Trap[C, B] => D)(implicit
         ev: Elem[B, HNil]
     ): D =
       sys.error("hmmm")
